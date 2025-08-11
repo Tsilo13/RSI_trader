7b3d22f4921723bac988c69bfb9aadf26e474bd6
Tsilo13 <168996878+Tsilo13@users.noreply.github.com>
2025-08-11 13:03:13 -0400

    created general directory for the bot

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Notes for the bot;
+1. Timeframe: Daily Bars
+2.RSI- 14 day 30/70
+3. Entry RSI crosses up from below 30
+4. Exit RSI crosses down below 70
+4. Instrument(what assets the bot trades):liquid ETFs like SPY and fractional shares.
+5. Stops: 
+"""
